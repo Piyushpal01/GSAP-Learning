3ba194ea6160da571bc4738f14dfb6639a6e76f2
Piyushpal01 <palp2406@gmail.com>
2025-04-20 01:43:59 +0530

    added comments

--- 02/script.js
+++ 02/script.js
@@ -1,4 +1,16 @@
-// scrollTrigger
+/*
+1. GSAP used for animate elements
+2. ScrollTrigger used for animate elements on basis of scrolling
+properties of scrollTrigger
+- trigger: 'select element you wants to trigger',
+-scroller: " body most of the times",
+-start : "animation start point",
+-end: "animation end point",
+-markers : " Boolean" -helps us to make perfect animtion  we can remove it later,
+-scrub: "Boolean or any no. between 1-5" -to make animation back and forth,
+-pin : pin the page while animating elements
+ ** note: while using pin property make sure trigger must be parent element 
+*/
 
 /*
 gsap.from("#page1 #box", {
